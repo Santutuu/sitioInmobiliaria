6f4e35b5801c51286b88cec1390f0268d4d65a80
Santutuu <santinoradix@gmail.com>
2024-06-11 12:01:09 -0300

    buscador con js

--- main/app.js
+++ main/app.js
@@ -1,22 +1,22 @@
 const propiedades = [
-    { ubicacion: ['Bariloche', 'San Carlos de Bariloche'], direccion: "Onelli 3277", precio: 460 , imagen: "/ciudades/imagenes/bariloche600.jpg", estrellas: 3, huespedes: 3 },
-    { ubicacion: ['Bariloche', 'San Carlos de Bariloche'], direccion: "Arelauquen lote 322", precio: 1000, imagen: "/ciudades/imagenes/bariloche1200.jpg", estrellas: 4, huespedes: 6 },
+    { ubicacion: ['Bariloche', 'San Carlos de Bariloche'], direccion: "Onelli 3277", precio: 460 , imagen: "/Inmobiliariahtml/ciudades/imagenes/bariloche600.jpg", estrellas: 3, huespedes: 3 },
+    { ubicacion: ['Bariloche', 'San Carlos de Bariloche'], direccion: "Arelauquen lote 322", precio: 1000, imagen: "/Inmobiliariahtml/ciudades/imagenes/bariloche1200.jpg", estrellas: 4, huespedes: 6 },
     { ubicacion: ['Bariloche', 'San Carlos de Bariloche'], direccion: "Arelauquen lote 322", precio: 700, imagen: "imagenes/casarticle.jpg" , estrellas: 5, huespedes: 6 },
     
     
-    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Lago el Condor 1233", precio: 1200, imagen: "/ciudades/imagenes/usuahia+1200.jpg", estrellas: 5, huespedes: 5 },
-    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Paseo el Sol nro 23", precio: 830, imagen: "/ciudades/imagenes/usuahia1000.jpg", estrellas: 3, huespedes: 5 },
-    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Bestchedt 1231", precio: 465, imagen: "/imagenes/ushuaia.jpg", estrellas: 3, huespedes: 5 },
+    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Lago el Condor 1233", precio: 1200, imagen: "/Inmobiliariahtml/imagenes/usuahia+1200.jpg", estrellas: 5, huespedes: 5 },
+    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Paseo el Sol nro 23", precio: 830, imagen: "/Inmobiliariahtml/ciudades/imagenes/usuahia1000.jpg", estrellas: 3, huespedes: 5 },
+    {ubicacion: ['Ushuaia', 'Ushuaia'], direccion: "Bestchedt 1231", precio: 465, imagen: "/Inmobiliariahtml/imagenes/ushuaia.jpg", estrellas: 3, huespedes: 5 },
     
-    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "Complejo barrial Amancay", precio: 700, imagen: "/main/imagenes/casa1Madryn.jpg", estrellas: 4, huespedes: 5 },
-    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "O Higgins 253", precio: 450, imagen: "/imagenes/buscador/madryn500.jpg", estrellas: 5, huespedes: 5 },
-    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "Puerto Moreno 45", precio: 1024, imagen: "/ciudades/imagenes/madryn200jpg.jpg", estrellas: 4, huespedes: 5 },
+    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "Complejo barrial Amancay", precio: 700, imagen: "/Inmobiliariahtml/main/imagenes/casa1Madryn.jpg", estrellas: 4, huespedes: 5 },
+    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "O Higgins 253", precio: 450, imagen: "/Inmobiliariahtml/imagenes/buscador/madryn500.jpg", estrellas: 5, huespedes: 5 },
+    { ubicacion: ['Puerto Madryn', 'Puerto Madryn'], direccion: "Puerto Moreno 45", precio: 1024, imagen: "/Inmobiliariahtml/ciudades/imagenes/madryn200jpg.jpg", estrellas: 4, huespedes: 5 },
 
     
-    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Bustillo 4344", precio: 750, imagen: "/ciudades/imagenes/sanmartin1000.jpg", estrellas: 4, huespedes: 3 },
-    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Antuhue 433", precio: 950, imagen: "/ciudades/imagenes/sanmartin10002.jpg", estrellas: 4, huespedes: 4 },
-    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Barrio las Victorias", precio: 1250, imagen: "/ciudades/imagenes/sanmartin+200.jpg", estrellas: 5, huespedes: 7 },
-    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Camino del Peludo 4500", precio: 499, imagen: "/main/ciudades/imagenes/Sanmartin3OG.jpg", estrellas: 5, huespedes: 3 },
+    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Bustillo 4344", precio: 750, imagen: "/Inmobiliariahtml/ciudades/imagenes/sanmartin1000.jpg", estrellas: 4, huespedes: 3 },
+    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Antuhue 433", precio: 950, imagen: "/Inmobiliariahtml/ciudades/imagenes/sanmartin10002.jpg", estrellas: 4, huespedes: 4 },
+    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Barrio las Victorias", precio: 1250, imagen: "/Inmobiliariahtml/ciudades/imagenes/sanmartin+200.jpg", estrellas: 5, huespedes: 7 },
+    { ubicacion: ['San Martin', 'San Martin de los Andes'], direccion: "Camino del Peludo 4500", precio: 499, imagen: "/Inmobiliariahtml/main/ciudades/imagenes/Sanmartin3OG.jpg", estrellas: 5, huespedes: 3 },
 
     
 ];
@@ -27,7 +27,6 @@ const propiedades = [
 var listaPrecio=[];
 
 var contador=0;
-var coincidencias=0;
 
 function guardar_valores() {
 
@@ -145,7 +144,7 @@ function mostrarPropiedades(propiedades) {
         clone.querySelector('.propiedad-direccion').textContent = `${propiedad.direccion}`;
         clone.querySelector('.propiedad-ubicacion').textContent = `${propiedad.ubicacion[1]}`;
 
-        let estrellasHTML = '';
+        
 
         clone.querySelector('.estrellas').innerHTML = cantidad_estrellas(propiedad.estrellas);
         
@@ -154,10 +153,12 @@ function mostrarPropiedades(propiedades) {
         contador++;
     });
 
-    coincidencias = propiedades.length; // Actualizar coincidencias después de mostrar propiedades
+    coincidencias = propiedades.length; 
     const alojamientosEncontrados = document.querySelector('.alojamientos-encontrados');
-    if (alojamientosEncontrados) {
+    if (coincidencias !== 0) {
         alojamientosEncontrados.textContent = `${coincidencias} propiedad/es encontradas`;
+    } else {
+        alert (`No se encontraron propiedades. Prueba buscando 'San Martin', 'Bariloche', 'Ushuaia', 'Puerto Madryn'`);
     }
 }
 
